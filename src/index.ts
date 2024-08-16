47f2f923553cd577db9909ea527bdc6ec704b647
Chris <81205126+TalentedB@users.noreply.github.com>
2024-08-16 01:13:44 -0700

    Removes dev tools

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ function createWindow(): void {
     });
 
     win.maximize();
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 
     win.loadURL(
         `file://${path.join(__dirname, '../src/index.html')}`
